7ad3549c46f68d1d2947122093e8c91e2f260d41
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-02 19:13:34 -0300

    ex 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const apiUrl = 'https://dog.ceo/api/breeds/image/random';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.fetchApi = this.fetchApi.bind(this);
+    this.nextDoguinho = this.nextDoguinho.bind(this);
+    this.state = {
+      loading: true,
+      imagem: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi(apiUrl);
+  }
+
+  // componentDidUpdate() {
+  //   this.fetchApi(apiUrl);
+  // }
+
+  fetchApi(api) {
+    fetch(api).then((r) => r.json()).then((r) => this.setState({ loading: false, imagem: r.message }));
+  }
+
+  nextDoguinho() {
+    this.setState({ loading: true },
+      () => this.fetchApi(apiUrl));
+  }
+
+  render() {
+    const { loading, imagem } = this.state;
+    if (loading === true) return <p>Loading...</p>
+    return (
+      <div>
+        <h1>Doguinho!</h1>
+        <img src={ imagem } alt='doguinho' />
+        <button type='button' onClick={ this.nextDoguinho }>Próximo doguinho!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
